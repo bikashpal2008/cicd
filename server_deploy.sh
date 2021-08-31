ea8d041ec225dc9ce576d9957541fdc0496fad7f
Bikash <bikash@codelogicx.com>
2021-08-31 10:00:06 +0530

    digital ocean check

--- server_deploy.sh
+++ server_deploy.sh
@@ -6,8 +6,7 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git fetch origin master
-    git reset --hard origin/master
+    git pull
 
     # Install dependencies based on lock file
     composer install --no-interaction --prefer-dist --optimize-autoloader
