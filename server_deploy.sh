656eb28635610a3a6b2356ee63b8c3678dbcf0cc
Bikash <bikash@codelogicx.com>
2021-08-31 10:14:48 +0530

    digital ocean check

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+#(php artisan serve --no-reload) || true
     # Update codebase
     git pull
 
@@ -12,7 +12,7 @@ echo "Deploying application ..."
     composer install --no-interaction --prefer-dist --optimize-autoloader
 
     # Migrate database
-    php artisan migrate --force
+    #php artisan migrate --force
 
     # Note: If you're using queue workers, this is the place to restart them.
     # ...
@@ -23,6 +23,6 @@ echo "Deploying application ..."
     # Reload PHP to update opcache
     echo "" | sudo -S service php8.0-fpm reload
 # Exit maintenance mode
-php artisan up
+# php artisan up
 
 echo "Application deployed!"
